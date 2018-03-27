eb09cba7ed27e328e14c154d2a3e7490906fb6d0
Ahmad aghazadeh <ahmad.aghazadeh.a@gmail.com>
2018-03-27 12:12:24 +0430

    Add request

--- index.js
+++ index.js
@@ -1,8 +1,12 @@
 var schedule = require('node-schedule');
+var request = require('request');
 
-var msg="Hello world";
-console.log(msg);
+console.log("Start");
 
 var j = schedule.scheduleJob('2 * * * * *',  function(fireDate){
     console.log('This job was supposed to run at ' + fireDate + ', but actually ran at ' + new Date());
+    request('http://tele.maxdroid.ir/', function (error, response, body) {
+        console.log('error:', error); // Print the error if one occurred
+        console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received
+    });
 });
